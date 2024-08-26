0b5faf42ac73ff3aad728be24ec4d1f815fee95a
Ian Gloude <igloude@gitlab.com>
2024-08-26 10:26:15 -0500

    feat(GlLink): Use Design Tokens in Link & Implement Styles to Spec
    - Implementing design tokens
    - Adding variant prop to GlLink
    - Adding variant styles to GlLink to conform to spec
    - Adding stories for each variant
    - Adding meta link story options
    - Updating existing stories with link variants
    
    Co-authored-by: Paul Gascou-Vaillancourt <pgascouvaillancourt@gitlab.com>
    Co-authored-by: Sascha Eggenberger <seggenberger@gitlab.com>

--- src/components/base/link/link.vue
+++ src/components/base/link/link.vue
@@ -2,6 +2,8 @@
 <script>
 import { BLink } from '../../../vendor/bootstrap-vue/src/components/link/link';
 import { SafeLinkMixin } from '../../mixins/safe_link_mixin';
+import { isExternalURL } from '../../../directives/safe_link/safe_link';
+import { linkVariantOptions } from '../../../utils/constants';
 
 export default {
   name: 'GlLink',
@@ -9,6 +11,50 @@ export default {
     BLink,
   },
   mixins: [SafeLinkMixin],
+  props: {
+    href: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    showExternalIcon: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    isCurrentUser: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    variant: {
+      type: String,
+      required: false,
+      default: linkVariantOptions.inline,
+      validator: (value) => Object.keys(linkVariantOptions).includes(value),
+    },
+  },
+  computed: {
+    isMentioningCurrentUser() {
+      return this.variant === linkVariantOptions.mention && this.isCurrentUser;
+    },
+    renderExternalIcon() {
+      return (
+        this.showExternalIcon &&
+        isExternalURL(this.target, this.href) &&
+        this.variant === linkVariantOptions.inline
+      );
+    },
+    linkClasses() {
+      const classes = [`gl-link gl-link-${this.variant}`];
+
+      if (this.isMentioningCurrentUser) {
+        classes.push('gl-link-mention-current');
+      }
+
+      return classes;
+    },
+  },
 };
 </script>
 <template>
@@ -16,10 +62,10 @@ export default {
     v-safe-link:[safeLinkConfig]
     v-bind="$attrs"
     :target="target"
-    class="gl-link"
+    :class="linkClasses"
     v-on="$listeners"
   >
     <!-- @slot The link to display. -->
-    <slot></slot>
+    <slot></slot><span v-if="renderExternalIcon">↗</span>
   </b-link>
 </template>
